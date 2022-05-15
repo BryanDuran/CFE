79be9481d398d6cf00424d26cfd7f79b1aba6fc7
Bryan Cuellar <56562805+BryanDuran@users.noreply.github.com>
2022-05-14 23:12:19 -0500

    Add files via upload
    14/mayo/2022

---
+++ ContratosCFE/src/test/java/mx/uv/t4is/ContratosCFE/ContratosCfeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.t4is.ContratosCFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratosCfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
