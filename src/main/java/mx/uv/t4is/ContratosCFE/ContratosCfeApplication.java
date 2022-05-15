7fd340504faa3f5e015e054ae074938006d4e12f
Bryan Cuellar <56562805+BryanDuran@users.noreply.github.com>
2022-05-14 23:13:42 -0500

    Add files via upload

---
+++ src/main/java/mx/uv/t4is/ContratosCFE/ContratosCfeApplication.java
@@ -0,0 +1,13 @@
+package mx.uv.t4is.ContratosCFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratosCfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratosCfeApplication.class, args);
+	}
+
+}
