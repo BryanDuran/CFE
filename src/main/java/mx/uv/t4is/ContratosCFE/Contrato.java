8db376d60ea9d5fc22ed57134fc0ec8b5f8341c9
Bryan Cuellar <56562805+BryanDuran@users.noreply.github.com>
2022-05-15 00:34:06 -0500

    Add files via upload

---
+++ src/main/java/mx/uv/t4is/ContratosCFE/Contrato.java
@@ -0,0 +1,71 @@
+package mx.uv.t4is.ContratosCFE;
+
+//nombre,domicilio,telefono,nContrato,pago,firmaE
+public class Contrato {
+
+    private String nombre;
+    private String domicilio;
+    private int telefono;
+    private int nContrato;
+    private int pago;
+    private String firmaE;
+
+    public Contrato() {
+
+    }
+
+    public String getNombre(){
+        return nombre;
+
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio){
+        this.domicilio = domicilio;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+    
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+
+    public int getNContrato() {
+        return nContrato;
+    }
+
+    public void setNContrato(int nContrato){
+        this.nContrato = nContrato;
+    }
+
+    public int getPago() {
+        return pago;
+    }
+
+    public void setPago(int pago){
+        this.pago = pago;
+    }
+
+    public String getFirmaE() {
+        return firmaE;
+    }
+    
+    public void setFirmaE(String firmaE){
+        this.firmaE = firmaE;
+    }
+    
+    
+   
+    
+    
+}
